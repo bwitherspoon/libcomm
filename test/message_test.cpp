ddaf61fdb2ad00e8e42d71bad015c44f0b1e5852
Brett Witherspoon <spoonb@cdspooner.com>
2015-10-04 19:39:52 -0500

    test: cleanup message test

--- test/message_test.cpp
+++ test/message_test.cpp
@@ -10,36 +10,42 @@
 BOOST_AUTO_TEST_CASE(message_test)
 {
   const bool b = true;
-  const uint8_t ui8 = 55;
-  const int8_t i8 = -9;
+  const uint8_t u8 = 55;
+  const int8_t s8 = -9;
   const float f32 = 9.0;
-  const int32_t i32 = -10000.7;
+  const int32_t s32 = -10000.7;
 
   bool b_r;
   uint8_t u8_r;
-  int8_t ui8_r;
+  int8_t s8_r;
   float f32_r;
-  int32_t i32_r;
+  int32_t s32_r;
 
   comm::message msg;
 
-  msg.serialize();
-  msg << b << ui8 << i8 << f32 << i32;
-  msg.serialize();
+  BOOST_REQUIRE_NO_THROW(msg.serialize());
+  BOOST_REQUIRE_NO_THROW(msg << b);
+  BOOST_REQUIRE_NO_THROW(msg << u8);
+  BOOST_REQUIRE_NO_THROW(msg << s8);
+  BOOST_REQUIRE_NO_THROW(msg << f32);
+  BOOST_REQUIRE_NO_THROW(msg << s32);
+  BOOST_REQUIRE_NO_THROW(msg.serialize());
 
   BOOST_REQUIRE_NO_THROW(msg.deserialize());
-
-  msg >> b_r >> u8_r >> ui8_r >> f32_r >> i32_r;
+  BOOST_REQUIRE_NO_THROW(msg >> b_r);
+  BOOST_REQUIRE_NO_THROW(msg >> u8_r);
+  BOOST_REQUIRE_NO_THROW(msg >> s8_r);
+  BOOST_REQUIRE_NO_THROW(msg >> f32_r);
+  BOOST_REQUIRE_NO_THROW(msg >> s32_r);
+  BOOST_REQUIRE_NO_THROW(msg.deserialize());
 
   BOOST_CHECK_EQUAL(b_r, b);
 
-  BOOST_CHECK_EQUAL(u8_r, ui8);
+  BOOST_CHECK_EQUAL(u8_r, u8);
 
-  BOOST_CHECK_EQUAL(ui8_r, i8);
+  BOOST_CHECK_EQUAL(s8_r, s8);
 
   BOOST_CHECK_EQUAL(f32_r, f32);
 
-  BOOST_CHECK_EQUAL(i32_r, i32_r);
-
-  BOOST_REQUIRE_NO_THROW(msg.deserialize());
+  BOOST_CHECK_EQUAL(s32_r, s32);
 }
