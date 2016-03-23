e11c9f40c3df1be26c45c7eb8657f77066c93e86
Brett Witherspoon <spoonb@cdspooner.com>
2016-03-22 23:55:22 -0500

    app: add options parser to usb_to_pipe

--- app/usb_to_pipe.cpp
+++ app/usb_to_pipe.cpp
@@ -14,21 +14,50 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <boost/program_options.hpp>
+
 #include <signum/usb.hpp>
 #include <signum/buffer.hpp>
 #include <signum/util/priority.hpp>
 
+namespace po = boost::program_options;
+
 int main (int argc, char *argv[])
 {
-    const uint16_t vid = 0x04b4;    // Netchip Technology, Inc
-    const uint16_t pid = 0x00f1;    // Linux-USB "Gadget Zero"
-    const std::string name = "/tmp/usb_to_pipe";
-    const int thresh = 1;
+    uint16_t vid;
+    uint16_t pid;
+    unsigned char ep;
+
+    po::options_description desc("Supported options");
+    desc.add_options()
+        ("help,h", "print help message")
+        ("vid,v", po::value<uint16_t>(&vid)->default_value(0x0525), "set vendor ID")
+        ("pid,p", po::value<uint16_t>(&pid)->default_value(0xa4a0), "set product ID")
+        ("ep,e", po::value<unsigned char>(&ep)->default_value(0x81), "set endpoint")
+        ("name,n", po::value<std::string>()->default_value("/tmp/usb_to_pipe"), "set name of pipe")
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
     std::atomic<size_t> sum(0);
 
     signum::util::set_realtime_priority();
 
-    signum::usb::source src(vid, pid, signum::usb::endpoint::EP6_IN);
+    if (mkfifo(vm["name"].as<std::string>().c_str(), 0666) == -1)
+        throw std::runtime_error("Unable to create a named pipe");
+
+    int fd = open(vm["name"].as<std::string>().c_str(), O_WRONLY);
+    if (fd == -1)
+        throw std::runtime_error("Unable to open the named pipe");
+
+    signum::usb::source src(vid, pid, static_cast<signum::usb::endpoint>(ep));
 
     std::thread usb([&]{ while(true) src(); });
     usb.detach();
@@ -57,16 +86,9 @@ int main (int argc, char *argv[])
 
     auto out = src.make_reader<unsigned char>();
 
-    if (mkfifo(name.c_str(), 0666) == -1)
-        throw std::runtime_error("Unable to create a named pipe");
-
-    int fd = open(name.c_str(), O_WRONLY);
-    if (fd == -1)
-        throw std::runtime_error("Unable to open the named pipe");
-
     while (true)
     {
-        out.wait(thresh);
+        out.wait(2048);
 
         size_t len = out.size();
 
