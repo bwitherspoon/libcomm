af299e24ab2bbcf4b63e213077a5c87344e2b524
Brett Witherspoon <spoonb@cdspooner.com>
2016-03-23 02:02:31 -0500

    app: usb_to_pipe: cleanup

--- app/usb_to_pipe.cpp
+++ app/usb_to_pipe.cpp
@@ -1,5 +1,5 @@
 /*
- *
+ * Copyright 2016 C. Brett Witherspoon
  */
 
 #include <iostream>
@@ -22,15 +22,40 @@
 
 namespace po = boost::program_options;
 
-// These calls block until reader is available
+void throughput_monitor(std::atomic<int> &sum)
+{
+    const char rot[] = { '-', '\\', '|', '/'};
+    int idx = 0;
+
+    signum::util::set_normal_priority();
+
+    std::cout << std::fixed << std::setprecision(2) << std::endl;
+
+    while (true)
+    {
+        sum = 0;
+
+        auto start = std::chrono::steady_clock::now();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> diff = end - start;
+
+        auto rate = sum / 2.0 / diff.count() / 1e6;
+
+        std::cout << "\r" << rot[idx] << " Rate: " << rate << " Msps " << rot[idx] << std::flush;
+
+        idx = (idx + 1) % 4;
+    }
+}
+
+// Blocks until reader is available
 int create_named_pipe(std::string name)
 {
     // Try to open the pipe first in case it exists
     int fd = open(name.c_str(), O_WRONLY);
-
-    // If open failed try to create the pipe
     if (fd == -1)
     {
+        // If open failed try to create the pipe and then open it
         if (mkfifo(name.c_str(), 0666) == -1)
             throw std::runtime_error("Unable to create a named pipe");
 
@@ -38,7 +63,6 @@ int create_named_pipe(std::string name)
         if (fd == -1)
             throw std::runtime_error("Unable to open the named pipe");
     }
-
     return fd;
 }
 
@@ -46,14 +70,14 @@ int main (int argc, char *argv[])
 {
     uint16_t vid;
     uint16_t pid;
-    unsigned char ep;
+    unsigned char end;
 
     po::options_description desc("Supported options");
     desc.add_options()
         ("help,h", "print help message")
         ("vid,v", po::value<uint16_t>(&vid)->default_value(0x0525), "set vendor ID")
         ("pid,p", po::value<uint16_t>(&pid)->default_value(0xa4a0), "set product ID")
-        ("ep,e", po::value<unsigned char>(&ep)->default_value(0x81), "set endpoint")
+        ("end,e", po::value<unsigned char>(&end)->default_value(0x81), "set endpoint")
         ("name,n", po::value<std::string>()->default_value("/tmp/usb_to_pipe"), "set name of pipe")
     ;
     po::variables_map vm;
@@ -66,43 +90,21 @@ int main (int argc, char *argv[])
         return 1;
     }
 
-    std::atomic<size_t> sum(0);
+    signum::util::set_realtime_priority();
 
     int fd = create_named_pipe(vm["name"].as<std::string>());
 
-    signum::util::set_realtime_priority();
+    // USB source
+    signum::usb::source src(vid, pid, static_cast<signum::usb::endpoint>(end));
 
-    signum::usb::source src(vid, pid, static_cast<signum::usb::endpoint>(ep));
+    std::thread([&]{ while(true) src(); }).detach();
 
-    std::thread usb([&]{ while(true) src(); });
-    usb.detach();
+    // Throughput monitor
+    std::atomic<int> sum(0);
 
-    std::thread throughput([&]{
-        const char rot[] = { '-', '\\', '|', '/'};
-        int idx = 0;
+    std::thread(throughput_monitor, std::ref(sum)).detach();
 
-        signum::util::set_normal_priority();
-
-        std::cout << std::fixed << std::setprecision(2) << std::endl;
-
-        while (true)
-        {
-            sum = 0;
-
-            auto start = std::chrono::steady_clock::now();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            auto end = std::chrono::steady_clock::now();
-            std::chrono::duration<double> diff = end - start;
-
-            auto rate = sum / 2.0 / diff.count() / 1e6;
-
-            std::cout << "\r" << rot[idx] << " Rate: " << rate << " Msps " << rot[idx] << std::flush;
-
-            idx = (idx + 1) % 4;
-        }
-    });
-    throughput.detach();
-
+    // Pipe sink
     auto out = src.make_reader<unsigned char>();
 
     while (true)
@@ -118,6 +120,4 @@ int main (int argc, char *argv[])
 
         sum += len;
     }
-
-    return 0;
 }
