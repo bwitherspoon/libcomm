d9b42904337689a52169f13137542496a5f1c214
Brett Witherspoon <spoonb@cdspooner.com>
2016-03-26 18:37:39 -0500

    endian: use static inline functions

--- include/signum/util/endian.hpp
+++ include/signum/util/endian.hpp
@@ -13,13 +13,13 @@ namespace signum
 namespace util
 {
 //! Convert big endian to host byte order
-inline uint64_t betoh(uint64_t val) { return be64toh(val); }
+static inline uint64_t betoh(uint64_t val) { return be64toh(val); }
 
 //! Convert big endian to host byte order
-inline uint32_t betoh(uint32_t val) { return be32toh(val); }
+static inline uint32_t betoh(uint32_t val) { return be32toh(val); }
 
 //! Convert big endian to host byte order
-inline uint16_t betoh(uint16_t val) { return be16toh(val); }
+static inline uint16_t betoh(uint16_t val) { return be16toh(val); }
 
 //! Convert big endian to host byte order
 int64_t betoh(int64_t val);
@@ -37,13 +37,13 @@ double betoh(double val);
 float betoh(float val);
 
 //! Convert host to big endian byte order
-inline uint64_t htobe(uint64_t val) { return htobe64(val); }
+static inline uint64_t htobe(uint64_t val) { return htobe64(val); }
 
 //! Convert host to big endian byte order
-inline uint32_t htobe(uint32_t val) { return htobe32(val); }
+static inline uint32_t htobe(uint32_t val) { return htobe32(val); }
 
 //! Convert host to big endian byte order
-inline uint16_t htobe(uint16_t val) { return htobe16(val); }
+static inline uint16_t htobe(uint16_t val) { return htobe16(val); }
 
 //! Convert host to big endian byte order
 int64_t htobe(int64_t val);
